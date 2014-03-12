593935d3906425f36a1f4aa533a6a02c32f38154
Martin Vonheim Larsen <martin@melvon.com>
2014-03-12 15:46:31 +0100

    Changed 'i' to 'number' in summing.py

--- summing/summing.py
+++ summing/summing.py
@@ -2,6 +2,6 @@ from sys import stdin
 
 # loop through all the input lines
 for line in stdin:
-    i = map(int, line.split()) # parse all integers on current line
+    numbers = map(int, line.split()) # parse all integers on current line
 
-    print sum(i)
+    print sum(numbers)
